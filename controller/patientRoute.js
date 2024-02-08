88bb06ec5d8f4b4fae158f53b05a93319af293f7
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-08 15:55:07 +0530

    search file created

--- controller/patientRoute.js
+++ controller/patientRoute.js
@@ -19,4 +19,11 @@ router.get("/view",async(req,res)=>{
     res.json(data)
 })
 
+router.post("/search",async(req,res)=>{
+    let input = req.body
+    let data = await patientModel.find(input)
+    res.json(data)
+
+})
+
 module.exports = router
